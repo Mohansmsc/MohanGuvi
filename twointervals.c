f69e91f1d7344c735286139f02c18b3c39750e76
Mohansmsc <mohanselvam1997@gmail.com>
2018-03-21 16:17:55 +0530

    Update twointervals.c

--- twointervals.c
+++ twointervals.c
@@ -1,33 +1,17 @@
 #include <stdio.h>
-
 int main(void) 
-
 {
-
 int start,end,i=0;
-
-printf("enter the  starting number");
-
+printf("enter starting number");
 scanf("%d",&start);
-
-printf("enter the ending number");
-
+printf("enter ending number");
 scanf("%d",&end);
-
 for(i=start+1;i<=end;i++)
-
 {
-
 if(i%2==0)
-
 {
-
 printf("%d",i);
-
 }
-
 }
-
 return 0;
-
 }
